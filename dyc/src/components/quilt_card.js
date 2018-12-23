4ed33d0a9c137cf82ae3ba42d22db112e0498d75
TrRo26 <travis_roy@yahoo.com>
2018-12-23 18:57:02 -0500

    Add CSS to quilt cards

--- dyc/src/components/quilt_card.js
+++ dyc/src/components/quilt_card.js
@@ -1,22 +1,17 @@
-// import PropTypes from 'prop-types'
 import React from 'react'
-// import Image1 from '../images/image_one.png'
+import './quilt_card.css'
 
 const QuiltCard = (props) => (
-    <div>
-        <div style={{ maxWidth: `700px`, marginBottom: `1.45rem` }}>
+    <div className='all-container'>
+        <div className='image-container'>
             <img src={props.myimage}></img>
         </div>
-        <div>
-            <div>Title: {props.title}</div>
-            <div>Date: {props.date}</div>
-            <div>About: {props.description}</div>
+        <div className='info-container'>
+            <div className='title'><span className='info-headers'>Title: </span>{props.title}</div>
+            <div className='date'><span className='info-headers'>Date: </span>{props.date}</div>
+            <div className='description'><span className='info-headers'>Description: </span>{props.description}</div>
         </div>
     </div>    
 )
 
 export default QuiltCard
-
-/* <div>Title: {this.props.title}</div>
-<div>Date: {this.props.quiltData.date}</div>
-<div>About: {this.props.quiltData.description}</div> */
